6a58ef98680399bf39da5cbdd496bfa8747e4e89
yonil01 <yonil.rojas@e-capture.co>
2022-09-21 09:21:59 -0500

    wdwd

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"foro-hotel/api"
 	"foro-hotel/internal/env"
+	"os"
+	"strconv"
 )
-func main()  {
+
+func main() {
 	c := env.NewConfiguration()
-	api.Start(c.App.Port, c.App.ServiceName, c.App.LoggerHttp, c.App.AllowedDomains)
+
+	port := os.Getenv("PORT")
+
+	puerto, err := strconv.Atoi(port)
+	if err != nil {
+		fmt.Println("Error fatal in asignature port")
+	}
+
+	api.Start(puerto, c.App.ServiceName, c.App.LoggerHttp, c.App.AllowedDomains)
 
 }
